8d746eedb447d11006c95f894987c792bda5b428
feelthesource <andy.lamb@yahoo.com>
2019-08-23 21:02:48 +0100

    Adds Append extension

---
+++ Extension.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace IEnumerable.Append
+{
+    public static class Extension
+    {
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> collection, T item)
+        {
+            return collection.Concat(Enumerable.Repeat(item, 1));
+        }
+    }
+}
